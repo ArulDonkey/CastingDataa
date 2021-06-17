19cae9974d8e25d548c298a748f48363127abfe3
Pahaerul Mutaqin <Pahaerul Mutaqin@DESKTOP-R2NUMDC>
2021-06-16 18:21:13 -0700

    DataType and VariableScope

---
+++ ArrayTes/src/Tespackage/Array.java
@@ -0,0 +1,16 @@
+package Tespackage;
+
+public class Array {
+	public static void main(String[]args) {
+		
+		int a[][]= {{1,3,4},{3,4,5}};
+		int b[][]= {{1,3,4},{3,4,5}};
+		
+		for (int i=0;i<a.length;i++) {
+			for (int j=0; j<a[i].length; j++) {
+				System.out.println((a[i][j]+b[i][j])+ " ");
+			}
+			System.out.println(" ");
+		}
+	}
+}
